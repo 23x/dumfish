40c109c3ca966a1309687cf6b44fb1625a265ca4
lawl <root@s3cr3t.de>
2011-09-30 20:51:55 +0200

    implemented /setkey /getkey and /masterpw, encrypted blowini storage works

--- dumfish.pl
+++ dumfish.pl
@@ -29,13 +29,87 @@ use Crypt::CBC;
 Xchat::register('dumfish', '0.1', 'Plugin for DUM encrytion');
 
 my $debug=1;
-my $xchatdir = Xchat::get_info('xchatdir');
-my $cfg = Config::IniFiles->new( -file => $xchatdir."/blow.ini" );
+my $cfg = Config::IniFiles->new( -file => get_config_path() );
 my $masterpw=0;
  
 Xchat::hook_server("PRIVMSG", 'decrypt_message', { priority => Xchat::PRI_HIGHEST });
-Xchat::hook_command( "",'encrypt_message', { priority => Xchat::PRI_LOWEST } );
+Xchat::hook_command("",'encrypt_message', { priority => Xchat::PRI_LOWEST } );
+Xchat::hook_command("MASTERPW",'set_masterpw');
+Xchat::hook_command("SETKEY",'set_key');
+Xchat::hook_command("GETKEY",'get_key');
+Xchat::hook_command("DELKEY",'del_key');
 
+dum_print("loaded");
+
+sub get_config_path {
+	return Xchat::get_info('xchatdir')."/blow.ini";
+}
+
+sub get_key {
+	if(!$masterpw){
+		dum_print("lrn2setmasterpassword");
+		return;
+	}
+	my $nick;
+	if(!$_[0][1]){
+		$nick=Xchat::get_info('channel');
+	}else{
+		$nick=$_[0][1];
+	}
+	my $key=get_dumkey($nick);
+	if($key){
+		dum_print("Key for ".$nick." is: ".get_dumkey($nick));
+	}else{
+		dum_print("No key set for: ".$nick);
+	}
+}
+
+sub set_key {
+	if(!$masterpw){
+		dum_print("lrn2setmasterpassword");
+		return;
+	}
+	my $nick;
+	my $pass;
+	if(!$_[0][2]){
+		$nick=Xchat::get_info('channel');
+		$pass=$_[0][1];
+	}else{
+		$nick=$_[0][1];
+		$pass=$_[0][2];
+	}
+	$cfg->newval($nick,'dumkey',dum_encrypt($pass,$masterpw));
+	if($cfg->WriteConfig(get_config_path())){
+		dum_print("Key for ".$nick." safed");
+		return;
+	}
+	dum_print("Couldn't safe key.");
+}
+
+
+sub del_key {
+	my $nick;
+	if(!$_[0][1]){
+		$nick=Xchat::get_info('channel');
+	}else{
+		$nick=$_[0][1];
+	}
+	$cfg->delval($nick,'dumkey');
+	if($cfg->WriteConfig(get_config_path())){
+		dum_print("Key for ".$nick." deleted");
+		return;
+	}
+	dum_print("Couldn't delete key.");
+}
+
+sub set_masterpw {
+	if($_[0][1] eq ""){
+		dum_print("lrn2usage: /MASTERPW <password>");
+		return;
+	}
+	$masterpw=$_[0][1];
+	dum_print("Master password set.")
+}
  
 sub decrypt_message {
 	my $servermessage=$_[1][0];
@@ -78,7 +152,7 @@ sub dum_decrypt {
 }
 
 sub get_dumkey {
-	$cfg->val( $_[0], 'dumkey' )
+	return dum_decrypt($cfg->val( $_[0], 'dumkey' ),$masterpw);
 }
 
 sub dum_encrypt {
@@ -86,9 +160,19 @@ sub dum_encrypt {
 	my $key=$_[1];
 	my $iv = Crypt::CBC->random_bytes(8);
 	my $cipher = Crypt::CBC->new(-key => $key,-iv => $iv,-cipher => 'Blowfish',-header => 'none');
-	return "+DUM ".encode_base64($iv.$cipher->encrypt($s));
+	return rm_newlines("+DUM ".encode_base64($iv.$cipher->encrypt($s)));
 }
 
 sub is_dum {
 	return index($_[0],"+DUM")!=-1?1:0;
+}
+
+sub dum_print {
+	Xchat::print("\x02dumfish\x02\t".$_[0]);
+}
+
+sub rm_newlines {
+	my $line=$_[0];
+	$line =~ s/\R//g;
+	return $line;
 }
